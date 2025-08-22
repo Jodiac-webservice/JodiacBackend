aef938ce61ffcc11b0ebe5ffa58b99d30dc7f64d
Ranit-dev2004 <saharanit16@gmail.com>
2025-08-22 17:12:20 +0530

    Fix casing in config file path and update secret key in auth configuration

--- Middlewares/authjwt.js
+++ Middlewares/authjwt.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const config = require("../Config/auth.config.js")
+const config = require("../config/Auth.config.js")
 const db = require("../models/index.model.js");
 
 verifyToken = (req, res, next) => {
