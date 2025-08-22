355950344ac5aa45daee7e03e64fdc929dd7ca0f
Ranit-dev2004 <saharanit16@gmail.com>
2025-08-22 17:23:49 +0530

    Fix casing in auth configuration file path

--- Middlewares/authjwt.js
+++ Middlewares/authjwt.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const config = require("../config/Auth.config.js")
+const config = require("../config/auth.config.js")
 const db = require("../models/index.model.js");
 
 verifyToken = (req, res, next) => {
