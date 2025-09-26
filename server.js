f37e336fa2abe7ac6255bbecdc613efeaec2d55a
Ranit-dev2004 <saharanit16@gmail.com>
2025-09-26 13:38:48 +0530

    fix ip

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ db.mongoose
   require("./Routes/payment.Routes")(app);
 
   const PORT = process.env.PORT || 4000;
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
   });
